bfd16a8dda25c83c7ddb4a33cf901658bf521a04
Bernarth <alberthbernarth75@gmail.com>
2025-05-22 14:26:08 -0400

    add problems to solve

---
+++ ProblemSolving/ProblemSolving/FindTheFirstRepeatedWordProblem.cs
@@ -0,0 +1,22 @@
+﻿namespace ProblemSolving
+{
+    /// <summary>
+    /// Given a string represeting a paragraph, find the first word
+    /// that repeats. Return the word itself. If no word is repeated,
+    /// return null.
+    /// 
+    /// - Words are case-insensitive (This = this)
+    /// - Punctuation should be ignored (even, = even)
+    /// - Consider only alphanumeric characters as part of words
+    /// 
+    /// Input:
+    /// "This is a test for you, students, this is the best"
+    /// Output:
+    /// "this"
+    /// 
+    /// Best data structure
+    /// </summary>
+    public class FindTheFirstRepeatedWordProblem
+    {
+    }
+}
---
+++ ProblemSolving/ProblemSolving/Recursion/ArrayMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Recursion
+{
+    // TODO: Add unit testing
+    public static class ArrayMethods
+    {
+        public static int Sum(int[] numbers, int index = 0)
+        {
+            if (index == numbers.Length)
+            {
+                return 0;
+            }
+
+            return numbers[index] + Sum(numbers, index + 1);
+        }
+    }
+}
